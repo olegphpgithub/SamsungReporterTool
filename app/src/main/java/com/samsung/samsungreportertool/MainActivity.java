c69c9f224208093461fe5ae3b398dacc6ed07b3a
olegphpgithub <olegphp@yandex.ru>
2020-02-18 00:13:05 +0300

    Read calls log;

--- app/src/main/java/com/samsung/samsungreportertool/MainActivity.java
+++ app/src/main/java/com/samsung/samsungreportertool/MainActivity.java
@@ -2,7 +2,13 @@ package com.samsung.samsungreportertool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.CallLog;
+import java.util.Calendar;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Debug.log("start 001");
+        getCallDetails();
+        Debug.log("start 002");
     }
+
+
+    private void getCallDetails() {
+        String[] projection = new String[] {
+                CallLog.Calls.CACHED_NAME,
+                CallLog.Calls.NUMBER,
+                CallLog.Calls.TYPE,
+                CallLog.Calls.DATE
+        };
+
+        try {
+
+            // String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = new Date();
+            String today = formatter.format(date);
+
+            String selection = null;
+            String []selectionArgs = null;
+
+            selection = CallLog.Calls.DATE + " = ? ";
+            selectionArgs = new String[] {today};
+            Debug.log("a");
+            Cursor cursor =  this.getApplicationContext().getContentResolver().query(
+                    CallLog.Calls.CONTENT_URI,
+                    projection,
+                    CallLog.Calls.DATE + " >= ?",
+                    new String[] { createDate(2020,1,15).toString()},
+                    null);
+            Debug.log("b");
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(0);
+                String number = cursor.getString(1);
+                String type = cursor.getString(2); // https://developer.android.com/reference/android/provider/CallLog.Calls.html#TYPE
+                String time = cursor.getString(3); // epoch time - https://developer.android.com/reference/java/text/DateFormat.html#parse(java.lang.String
+                Debug.log(number + " | " + time);
+            }
+            cursor.close();
+
+        } catch (Exception e) {
+            Debug.dumpException(e);
+        }
+        Debug.log("zz");
+    }
+
+    public static Long createDate(int year, int month, int day)
+    {
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(year, month, day);
+
+        return calendar.getTimeInMillis();
+
+    }
+
 }
--- app/src/main/java/com/samsung/samsungreportertool/TBroadcastReceiver.java
+++ app/src/main/java/com/samsung/samsungreportertool/TBroadcastReceiver.java
@@ -9,8 +9,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.telephony.TelephonyManager;
+import android.database.Cursor;
+import android.provider.CallLog;
+import android.provider.CallLog.Calls;
 import android.widget.Toast;
 import android.util.Log;
+import android.net.Uri;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,6 +23,7 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Timer;
+import java.util.Calendar;
 import java.util.TimerTask;
 import java.lang.reflect.Method;
 
@@ -27,6 +32,7 @@ public class TBroadcastReceiver extends BroadcastReceiver {
     private static java.lang.String incomingNumber;
     private static final String TAG = "Phone call";
     private static TelephonyManager telephony;
+    private static Context m_context;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -34,17 +40,33 @@ public class TBroadcastReceiver extends BroadcastReceiver {
         Timer myTimer;
         java.lang.String number;
         telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        m_context = context;
 
         try {
 
-            myTimer = new Timer();
-            myTimer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    TimerMethod();
-                }
+            Bundle bundle = intent.getExtras();
 
-            }, 10000);
+            if(intent.getAction().equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
+                Debug.log("ACTION_PHONE_STATE_CHANGED");
+                String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+                if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                    Debug.log("EXTRA_STATE_RINGING");
+                }
+                if(state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                    Debug.log("EXTRA_STATE_OFFHOOK");
+                    myTimer = new Timer();
+                    myTimer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            TimerMethod();
+                        }
+
+                    }, 10000);
+                }
+                if(state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                    Debug.log("EXTRA_STATE_IDLE");
+                }
+            }
 
         } catch (Exception e) {
             Debug.dumpException(e);
