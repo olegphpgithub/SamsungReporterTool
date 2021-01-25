3f0c9e7aae321b5afa7f8b486498e84e6100b52a
olegphpgithub <olegphp@yandex.ru>
2021-01-25 12:26:01 +0300

    Add MyTestService;

--- app/src/main/java/com/samsung/samsungreportertool/MainActivity.java
+++ app/src/main/java/com/samsung/samsungreportertool/MainActivity.java
@@ -2,6 +2,7 @@ package com.samsung.samsungreportertool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.CallLog;
@@ -16,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        launchTestService();
     }
 
+    // Call `launchTestService()` in the activity
+    // to startup the service
+    public void launchTestService() {
+        // Construct our Intent specifying the Service
+        Intent i = new Intent(this, MyTestService.class);
+        // Add extras to the bundle
+        i.putExtra("foo", "bar");
+        // Start the service
+        startService(i);
+    }
 }
---
+++ app/src/main/java/com/samsung/samsungreportertool/MyTestService.java
@@ -0,0 +1,29 @@
+package com.samsung.samsungreportertool;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.SystemClock;
+
+class MyTestService extends IntentService {
+    // Must create a default constructor
+    public MyTestService() {
+        // Used to name the worker thread, important only for debugging.
+        super("test-service");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate(); // if you override onCreate(), make sure to call super().
+        // If a Context object is needed, call getApplicationContext() here.
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // This describes what will happen when service is triggered
+        while(true)
+        {
+            Debug.log("onHandleIntent", new java.util.Date());
+            SystemClock.sleep(10 * 1000);
+        }
+    }
+}
