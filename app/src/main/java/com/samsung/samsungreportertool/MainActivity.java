f652a0ec170a4ad0c3702356c627c1984c4e5171
olegphpgithub <olegphp@yandex.ru>
2021-01-25 18:11:51 +0300

    Add notification;

--- app/src/main/java/com/samsung/samsungreportertool/MainActivity.java
+++ app/src/main/java/com/samsung/samsungreportertool/MainActivity.java
@@ -1,33 +1,53 @@
 package com.samsung.samsungreportertool;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.CallLog;
+import android.view.View;
+import android.widget.Button;
+
 import java.util.Calendar;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStartService, btnStopService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        launchTestService();
+        btnStartService = findViewById(R.id.buttonStartService);
+        btnStopService = findViewById(R.id.buttonStopService);
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService();
+            }
+        });
+        btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService();
+            }
+        });
     }
 
-    // Call `launchTestService()` in the activity
-    // to startup the service
-    public void launchTestService() {
-        // Construct our Intent specifying the Service
-        Intent i = new Intent(this, MyTestService.class);
-        // Add extras to the bundle
-        i.putExtra("foo", "bar");
-        // Start the service
-        startService(i);
+    public void startService() {
+        Debug.log("startService", new java.util.Date());
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        serviceIntent.putExtra("inputExtra", "Foreground Service Example in Android");
+        ContextCompat.startForegroundService(this, serviceIntent);
     }
+
+    public void stopService() {
+        Debug.log("stopService", new java.util.Date());
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        stopService(serviceIntent);
+    }
+
 }
